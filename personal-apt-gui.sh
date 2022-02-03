7600cafb263e65109fc7b721cc7e643d7001202a
Eduardo Pignatelli <edu.pignatelli@gmail.com>
2022-02-03 13:01:57 +0000

    Create personal-apt-gui.sh

---
+++ personal-apt-gui.sh
@@ -0,0 +1,71 @@
+function update_repos {
+    sudo apt-get update
+    sudo apt update
+}
+
+function install_rdp {
+    # install xfce4
+    sudo apt-get update
+    sudo apt-get -y install xfce4
+    sudo apt-get -y install xfce4-session
+
+    # install xrpd
+    sudo apt-get -y install xrdp
+    sudo systemctl enable xrdp
+
+    # set desktop environment manager on rdp
+    echo xfce4-session > ~/.xsession
+
+    # restart xrpd for chances to have effect
+    sudo service xrdp restart
+}
+
+function install_git {
+    # install git
+    sudo apt-get -y install git
+}
+
+function install_anaconda {
+    # exit if anaconda already installed
+    if [ ! -d ~/anaconda3 ]; then
+        # install anaconda prerequisites
+        sudo apt-get -y install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+        
+        # download and install anaconda
+        filename=$(curl https://repo.anaconda.com/archive/ | grep  Linux-x86_64 | sed -n '$p' | grep -o '<a .*href=.*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d')
+        url=https://repo.anaconda.com/archive/$filename
+        wget $url
+        sudo chmod +x $filename
+        bash $filename -b  -p $HOME/anaconda3
+        rm $filename        
+    fi
+
+    # activate shell
+    source $HOME/anaconda3/etc/profile.d/conda.sh
+    
+    # activate on login
+    echo "source $HOME/anaconda3/etc/profile.d/conda.sh" >> ~/.bashrc
+}
+
+function install_chrome {
+    # download chrome
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo apt-get -y install ./google-chrome-stable_current_amd64.deb
+}
+
+function install_ohmyzsh {
+    wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -
+}
+
+function install_tilda {
+    sudo apt-get -y install tilda
+}
+
+update_repos
+install_git
+install_anaconda
+install_ohmyzsh
+install_tilda
+install_chrome
+install_firefox
+install_rdp
